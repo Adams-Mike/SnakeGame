3fc7ef5477d690df9f95f2e2733f72fb936f26e7
Michael Adams <magnavoid@gmail.com>
2014-05-08 20:08:06 -0600

    Commit 1

--- SnakeGame/src/snakegame/SnakeGame.java
+++ SnakeGame/src/snakegame/SnakeGame.java
@@ -6,22 +6,41 @@
 
 package snakegame;
 
+import java.util.Scanner;
+
 /**
  *
  *
  */
 public class SnakeGame {
 
+    String name;
+    String instructions = "This is a game about a snake that is inside you. \n\n"
+            + "You will be playing against a random computer foe.\n"
+            + "The object of this game is to collect as many items as possible"
+            + " and survive.\n"
+            + "Use arrow keys or WASD to control the movement of the snake.\n"
+            + "Try not to die, and collect as many points as you can.\n\n"
+            + "GOOD LUCK!\n\n";
+    
+        
     public static void main(String[] args) {
         
-        int age = 0;
-        int year = 0;
+        SnakeGame myGame = new SnakeGame();
+        myGame.getName();
+        myGame.displayHelp();
         
-        
-        
-        
-    
     }
     
+    public void getName(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name:  ");
+        this.name = input.next();
+                
+    }
     
+    public void displayHelp() {
+        System.out.println("\nWelcome " + this.name + "\n");
+        System.out.println(this.instructions);
+    }
 }
