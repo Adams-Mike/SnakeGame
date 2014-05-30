a81ccdac714249606c1ba96cc70062d6e935e009
Michael Adams <magnavoid@gmail.com>
2014-05-29 22:15:19 -0600

    Commit by: Mike

---
+++ SnakeGame/src/snakegame/SnakeLengthView.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package snakegame;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Michael
+ */
+public class SnakeLengthView {
+    int minLength = 3;
+    int maxLength = 15;
+    int length = minLength;
+
+    public void snakeLength(){
+        boolean correct = false;
+        Scanner keyboard = new Scanner(System.in);
+        this.length = keyboard.nextInt();
+         while(!correct) {
+            if (length > maxLength || length < minLength){
+                System.out.println("Error, wrong input (3-15)");
+                this.length = keyboard.nextInt();
+                
+            }
+         }
+      }
+}
