ad179b2275e999734f06c0baeda183e36ec64eba
Michael Adams <magnavoid@gmail.com>
2014-07-03 20:03:37 -0600

    Commit by: Mike

---
+++ SnakeGame/src/snakegame/menus/HelpMenuControl.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakegame.menus;
+
+public class HelpMenuControl extends SuperMenu {
+    
+    public HelpMenuControl() {
+        super();
+    } 
+}
